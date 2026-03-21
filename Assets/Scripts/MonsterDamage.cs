db41a72c4df25c36a2181e133b942fe2e854a61c
Stanisław Kaczmarski <st.kaczmarski@wp.pl>
2026-03-21 18:32:27 +0100

    Add an enemy with animation, movement and collision-based damage

---
+++ Assets/Scripts/MonsterDamage.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MonsterDamage : MonoBehaviour
+{
+    public int damage;
+    public PlayerHealth playerHealth;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+}
