739d9ab593c500e24c74cd03d2c5362ce2717900
Scarecat <scarecat@tuta.io>
2026-03-22 13:24:28 +0100

    Monster: automatically find PlayerHealth

--- Assets/Scripts/MonsterDamage.cs
+++ Assets/Scripts/MonsterDamage.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class MonsterDamage : MonoBehaviour
 {
     public int damage;
-    public PlayerHealth playerHealth;
+
+    private PlayerHealth playerHealth;
+
+
+    void Start() {
+      playerHealth = GameObject.Find("Player").GetComponent<PlayerHealth>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
