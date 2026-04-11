c4d8a46ec1b1d5b3703ce55be156634e1472a8ee
scarecat <scarecat@tuta.io>
2026-04-11 21:35:21 +0200

    Add setting spawning level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,48 +4,71 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    private string currentLevel = null;
+  [SerializeField] private GameObject player;
 
-    private IEnumerator StartLoadLevel(string levelName, string entryPointName) {
-      player.GetComponent<PlayerHealth>().enabled = false;
-      player.GetComponent<PlayerMovement>().enabled = false;
-      player.GetComponent<SpriteRenderer>().enabled = false;
-      if (currentLevel != null) {
-        yield return SceneManager.UnloadSceneAsync(currentLevel);
-      }
-      var loadOperation = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
-      yield return loadOperation;
-      currentLevel = levelName;
-      
-      var spawnPos = GameObject.Find(entryPointName).transform;
-      player.transform.SetPositionAndRotation(spawnPos.position, Quaternion.identity);
+  [SerializeField] private string forceLevelToStartIn = null;
+  [SerializeField] private string forceEntryPointToStartIn = null;
+  private string currentLevel = null;
 
-      player.GetComponent<PlayerHealth>().enabled = true;
-      player.GetComponent<PlayerMovement>().enabled = true;
-      player.GetComponent<SpriteRenderer>().enabled = true;
+  private IEnumerator StartLoadLevel(string levelName, string entryPointName)
+  {
+    player.GetComponent<PlayerHealth>().enabled = false;
+    player.GetComponent<PlayerMovement>().enabled = false;
+    player.GetComponent<SpriteRenderer>().enabled = false;
+    if (currentLevel != null)
+    {
+      yield return SceneManager.UnloadSceneAsync(currentLevel);
     }
+    var loadOperation = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+    yield return loadOperation;
+    currentLevel = levelName;
+
+    var spawnPos = GameObject.Find(entryPointName).transform;
+    player.transform.SetPositionAndRotation(spawnPos.position, Quaternion.identity);
+
+    player.GetComponent<PlayerHealth>().enabled = true;
+    player.GetComponent<PlayerMovement>().enabled = true;
+    player.GetComponent<SpriteRenderer>().enabled = true;
+  }
 
 
 
-    public void LoadLevel(string levelName, string entryPointName = null) {
-      string spawnPointName = entryPointName ?? $"From{currentLevel}";
-      StartCoroutine(StartLoadLevel(levelName, spawnPointName));
+  public void LoadLevel(string levelName, string entryPointName = null)
+  {
+    string spawnPointName;
+
+    if (string.IsNullOrEmpty(entryPointName))
+    {
+      spawnPointName = $"From{currentLevel}";
+
     }
+    else
+    {
+      spawnPointName = entryPointName;
+    }
+    StartCoroutine(StartLoadLevel(levelName, spawnPointName));
+  }
 
 
 
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    if (!string.IsNullOrEmpty(forceLevelToStartIn))
+    {
+      LoadLevel(forceLevelToStartIn, forceEntryPointToStartIn);
+    }
+    else
     {
       LoadLevel("Level1", "PlayerStart");
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
