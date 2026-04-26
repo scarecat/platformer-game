7069503aace3f52763a58e6ebbf3f198bb8f8f6e
Jacek Jarosz <jacekjarosz55@gmail.com>
2026-04-26 14:36:29 +0200

    WIP add mechanical spike

--- Assets/Scripts/SpikesTilemap.cs
+++ Assets/Scripts/SpikesTilemap.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+[RequireComponent(typeof(Tilemap))]
+[RequireComponent(typeof(TilemapCollider2D))]
 public class SpikesTilemap : MonoBehaviour
 {
-    
     [SerializeField] private float damage = 10.0f;
+
+    private void Start()
+    {
+    }
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(!collision.TryGetComponent(out PlayerHealth health)) return;
-        
+        if (!collision.TryGetComponent(out PlayerHealth health)) return;
+
+
+
         health.TrapDamage(damage);
+
+
+
     }
 }
