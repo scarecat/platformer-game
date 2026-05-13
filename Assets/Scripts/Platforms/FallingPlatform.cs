ff8803c55f1a07ff89dde96653a4c541850643bc
scarecat <scarecat@tuta.io>
2026-05-13 22:51:44 +0200

    add falling platforms

---
+++ Assets/Scripts/Platforms/FallingPlatform.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+[RequireComponent(typeof(Animator))]
+public class FallingPlatform : MonoBehaviour
+{
+    [SerializeField] private float recoveryTime = 8.0f;
+    private static readonly int FallHash = Animator.StringToHash("Fall");
+    private static readonly int RecoverHash = Animator.StringToHash("Recover");
+    private Animator animator;
+    private bool interacted = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private IEnumerator RecoveryCoroutine()
+    {
+        yield return new WaitForSeconds(recoveryTime);
+        animator.Play(RecoverHash);
+        interacted = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (interacted || !collision.collider.CompareTag("Player")) return;
+        interacted = true;
+        animator.Play(FallHash);
+        StartCoroutine(RecoveryCoroutine());
+    }
+}
